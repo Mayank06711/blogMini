a7b6e34212642954ce754d5a1bd806c10a85f927
Mayank06711 <mayank93soni@gmail.com>
2024-04-13 02:10:01 +0530

    mongodb connected

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dotenv from "dotenv" // As early as possible in your application, import and configure dotenv: this is our first file which will be loaded 
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+//  it ensures that the environment variables are loaded before your application starts, making them available in your application code.
+dotenv.config({
+    path: "./.env",
+  }); // we have add this in package json file under dev -r dotenv/config --experimental-json-modules
+  
+
+
+connectDB()
